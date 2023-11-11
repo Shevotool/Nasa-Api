74d5d7b282153aa65d51ca94ba9d796ebe8cc441
Andrés González <89161075+Shevotool@users.noreply.github.com>
2023-11-10 22:51:44 -0600

    Mars

--- js/mars.js
+++ js/mars.js
@@ -1,7 +1,7 @@
 const content = document.querySelector(".mars-content");
 const apiKey = "8GNC8ah2YfQVedqCMw8Gg59zKlhiijtfQfffHz65";
 
-const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${apiKey}`;
+const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=4000&api_key=${apiKey}`;
 
 document.addEventListener("DOMContentLoaded", getData);
 
@@ -13,16 +13,17 @@ async function getData() {
   result.photos.forEach((element) => {
     console.log(element);
 
-    const { id, camera, img_src } = element;
+    const { id, camera, img_src, earth_date } = element;
 
-    const { name } = camera;
+    const { full_name } = camera;
 
     const divEl = document.createElement("div");
     divEl.classList.add("item-element");
 
     divEl.innerHTML = `
     <img src="${img_src}" class="camera-img" alt="Camera"></img>
-    <p class="name">Camera: ${name}</p>
+    <p class="name">Camera: ${full_name}</p>
+    <p class="name">Date: ${earth_date}</p>
     `;
 
     content.appendChild(divEl);
